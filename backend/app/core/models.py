2f0a9a512b5719255cba0cf3af2dc27b9341f5aa
wangonya <kwangonya@gmail.com>
2019-12-26 12:24:53 +0300

    set email and password required

--- backend/app/core/models.py
+++ backend/app/core/models.py
@@ -5,9 +5,11 @@ from django.contrib.auth.models import (
 
 
 class UserManager(BaseUserManager):
-    def create_user(self, email, password=None, **extra_fields):
+    def create_user(self, email, password, **extra_fields):
         """Creates and saves a new user"""
-        user = self.model(email=self.normalize_email(email), **extra_fields)
+        if not (email and password):
+            raise ValueError("User must have an email and password")
+        user = self.model(email=self.normalize_email(email), password=password, **extra_fields)
         user.set_password(password)
         user.save()
 
@@ -16,10 +18,11 @@ class UserManager(BaseUserManager):
 
 class User(AbstractBaseUser, PermissionsMixin):
     """Custom user model that supports using email instead of username"""
-    email = models.EmailField(max_length=100, unique=True)
+    email = models.EmailField(max_length=100, unique=True, null=False)
     name = models.CharField(max_length=50)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
+    password = models.CharField(null=False)
 
     objects = UserManager()
 
