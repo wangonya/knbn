50c194a3edd615dcd0acd1f9ac9df8e0e368dcd3
wangonya <kwangonya@gmail.com>
2020-01-03 18:34:18 +0300

    solve cors issues

--- backend/app/app/settings.py
+++ backend/app/app/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+CORS_ORIGIN_WHITELIST = ['http://localhost:3000']
 
 
 # Application definition
