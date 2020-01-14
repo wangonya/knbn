e4a41f94c566ba05e639cd16008be833fe4edf52
wangonya <kwangonya@gmail.com>
2020-01-14 19:25:10 +0300

    setup post, and alert interceptors

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+import { loadingAlert, successAlert, errorAlert } from './alerts';
+
 const SERVER_DOMAIN = process.env.REACT_APP_API_ROOT;
 
 const getHeaders = () => ({
@@ -8,6 +10,12 @@ const getHeaders = () => ({
     'Content-Type': 'application/json',
   },
 });
+
+// Add a request interceptor to add loading indicator on each request
+axios.interceptors.request.use((config) => {
+  loadingAlert('Loading...');
+  return config;
+}, (error) => Promise.reject(error));
 
 export const get = (path) => new Promise((resolve, reject) => {
   axios.get(`${SERVER_DOMAIN}/${path}/`, getHeaders())
@@ -21,11 +29,19 @@ export const patch = (path, data) => new Promise((resolve, reject) => {
     .catch((error) => { reject(console.log(error)); });
 });
 
-export const post = (path, data) => new Promise((resolve, reject) => {
+export const post = (path, data) => {
   axios.post(`${SERVER_DOMAIN}/${path}/`, data, getHeaders())
-    .then((response) => { resolve(response); })
-    .catch((error) => { reject(console.log(error)); });
-});
+    .then((response) => {
+      console.log(response.data);
+    })
+    .catch((error) => {
+      errorAlert(
+        error.response.data.error
+        || Object.values(error.response.data)[0]
+        || 'Something went wrong. Please try again later.',
+      );
+    });
+};
 
 export const del = (path) => new Promise((resolve, reject) => {
   axios.delete(`${SERVER_DOMAIN}/${path}/`, getHeaders())
