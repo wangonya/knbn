7ae569099a3ded26a336b47a81cb6565c05615dc
wangonya <kwangonya@gmail.com>
2020-01-28 20:31:02 +0300

    update model

--- backend/app/projects/models.py
+++ backend/app/projects/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+from core.models import User
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=100, null=False)
+    description = models.TextField()
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
