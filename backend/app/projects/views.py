c4eb03c40cb539977eb15ec693c275f195e9e66b
wangonya <kwangonya@gmail.com>
2020-01-28 20:31:38 +0300

    add get and post method for projects endpoint

--- backend/app/projects/views.py
+++ backend/app/projects/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import (
+    status,
+    views,
+    mixins,
+    generics,
+)
 
-# Create your views here.
+from .models import Project
+from .serializers import ProjectSerializer
+
+
+class ProjectList(mixins.ListModelMixin,
+                  mixins.CreateModelMixin,
+                  generics.GenericAPIView,
+                  ):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
