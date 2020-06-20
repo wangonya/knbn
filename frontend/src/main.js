20ab95727941fc042623de112428c0774b70ba2c
wangonya <kwangonya@gmail.com>
2020-06-20 16:16:11 +0300

    elm --again

--- frontend/src/main.js
+++
@@ -1,9 +0,0 @@
-import 'ant-design-vue/dist/antd.min.css'
-import Vue from 'vue'
-import App from './App.vue'
-
-Vue.config.productionTip = false
-
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
