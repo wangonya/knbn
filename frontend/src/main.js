0004bd69e25ad1d2630e261496d69c4a7f7fba79
wangonya <kwangonya@gmail.com>
2020-06-20 13:57:23 +0300

    add vue

---
+++ frontend/src/main.js
@@ -0,0 +1,9 @@
+import 'ant-design-vue/dist/antd.min.css'
+import Vue from 'vue'
+import App from './App.vue'
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
